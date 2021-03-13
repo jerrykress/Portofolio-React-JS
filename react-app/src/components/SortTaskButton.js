1afac9cd6a94038db42ed26cb95f1ac445edd11f
jerrykress <jerrykress.wsj@gmail.com>
2021-03-13 19:53:17 +0000

    Sort Button Minor Update

--- react-app/src/components/SortTaskButton.js
+++ react-app/src/components/SortTaskButton.js
@@ -33,7 +33,7 @@ const SortTaskButton = (props) => {
 
     return (
         <div className="relative" onMouseOver={showDropdown} onMouseLeave={hideDropdown}>
-            <button className="inline-flex items-center h-10 px-5 text-gray-400 border border-indigo-100 transition-colors duration-150 bg-white rounded-lg focus:outline-none hover:border-indigo-500 hover:text-indigo-700">
+            <button className="inline-flex items-center h-10 px-5 py-4 mb-3 text-gray-400 border border-indigo-100 transition-colors duration-150 bg-white rounded-lg focus:outline-none hover:border-indigo-500 hover:text-indigo-700">
                 <span>Sort by</span>
                 <svg className="w-4 h-4 ml-3 fill-current" viewBox="0 0 20 20"><path d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clipRule="evenodd" fillRule="evenodd"></path></svg>
             </button>
@@ -42,7 +42,7 @@ const SortTaskButton = (props) => {
                 <div className="absolute flex flex-col py-2 mt-0 text-gray-700 bg-white border rounded-lg shadow-lg">
                     <div className="px-3 py-1 hover:text-gray-800 focus:bg-indigo-700 hover:text-indigo-700" onClick={sortByDueTime}>Due First</div>
                     <div className="px-3 py-1 hover:text-gray-800 focus:bg-indigo-700 hover:text-indigo-700" onClick={sortByPriority}>High Priority</div>
-                    <div className="px-3 py-1 hover:text-gray-800 focus:bg-indigo-700 hover:text-indigo-700" onClick={sortByTag}>Project</div>
+                    {/* <div className="px-3 py-1 hover:text-gray-800 focus:bg-indigo-700 hover:text-indigo-700" onClick={sortByTag}>Project</div> */}
                 </div>
             }
         </div>
